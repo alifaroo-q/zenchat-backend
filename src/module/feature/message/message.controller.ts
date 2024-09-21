930ab2fdcb22d7f75ee4f0a29976411cca43fd60
Ali Farooq <alifarooq122@gmail.com>
2024-09-22 00:21:22 +0500

    added jwt auth guard to messages

--- src/module/feature/message/message.controller.ts
+++ src/module/feature/message/message.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { MessageService } from './message.service';
+import { JwtAuthGuard } from 'src/module/core/auth/guard/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('messages')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
